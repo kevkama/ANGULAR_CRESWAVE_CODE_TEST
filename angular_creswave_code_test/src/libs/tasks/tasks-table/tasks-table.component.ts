fda4eb91f325fae84588c2b71add3832b7da6243
Jane Njoroge <jwnjoroge4@gmail.com>
2024-05-09 17:52:22 +0300

    :wrench: created folder structure

---
+++ angular_creswave_code_test/src/libs/tasks/tasks-table/tasks-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasks-table.component.html',
+  styleUrl: './tasks-table.component.scss'
+})
+export class TasksTableComponent {
+
+}
---
+++ angular_creswave_code_test/src/libs/tasks/tasks.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { TasksRoutingModule } from './tasks-routing.module';
+// import { TaskTableComponent } from '../components/task-table/task-table.component';
+// import { TaskDetailsComponent } from '../components/task-details/task-details.component';
+// import { TaskEditComponent } from '../components/task-edit/task-edit.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { TasksTableComponent } from './tasks-table/tasks-table.component';
+
+@NgModule({
+  declarations: [
+    TasksTableComponent,
+    // TaskEditComponent
+  ],
+  imports: [
+    CommonModule,
+    // TasksRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatIconModule,
+    
+  ]
+})
+export class TasksModule { }
